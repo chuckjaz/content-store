2266460d31612df41e920b2d971b277536e9a7df
Chuck Jazdzewski <chuckj@google.com>
2016-05-05 15:28:51 -0700

    Moved to using async await and es6 emit.

--- src/content-store.ts
+++ src/content-store.ts
@@ -55,11 +55,11 @@ export class ContentStore {
    * @param filename the file to enter into the content store.
    * @returns a promise for the hash of the file entered into the store.
    */
-  enterFile(filename: string): Promise<string> {
+  async enterFile(filename: string): Promise<string> {
     let fullCacheName: string;
-    return this.hasher.hashOf(filename).then(hash => {
-      return this.enterHashedFile(filename, hash).then(() => hash);
-    });
+    let hash = await this.hasher.hashOf(filename);
+    await this.enterHashedFile(filename, hash);
+    return hash;
   }
   
   /**
@@ -69,11 +69,11 @@ export class ContentStore {
    * @param the directory to enter into the content store.
    * @returns a promise for the hash of the directory entered into the store.
    */
-  enterDirectory(directory: string): Promise<string> {
-    return this.hasher.hashDir(directory).then(results => {
-      const hash = this.hasher.hashEntries(results)
-      return this.enterHashedDirectory(directory, results, hash).then(() => hash);
-    });
+  async enterDirectory(directory: string): Promise<string> {
+    let results = await this.hasher.hashDir(directory);
+    let hash = this.hasher.hashEntries(results);
+    await this.enterHashedDirectory(directory, results, hash);
+    return hash;
   }
   
   /**
@@ -82,19 +82,13 @@ export class ContentStore {
    * @param location of the file or directory to link.
    * @returns a promise resolved when the file or directory is linked
    */
-  realize(location: string, hash: string): Promise<string> {
+  async realize(location: string, hash: string): Promise<string> {
     const fullHashName = this.cacheNameOfHash(hash);
-    return existsp(fullHashName).then(exists => {
-      if (!exists) {
-        throw new Error(`No cache entry for hash ${hash}`);
-      }
-      else {
-        // Realize the entry by link'ing the cache entry to the
-        // given name.
-        // TODO: If this fails we should symlink instead.
-        return link(fullHashName, location);
-      }
-    }).then(() => location);
+    if (!(await existsp(fullHashName))) {
+      throw new Error(`No cache entry for hash ${hash}`);
+    }
+    await link(fullHashName, location);
+    return location;
   }
   
   /**
@@ -116,31 +110,28 @@ export class ContentStore {
     if (this.entering[hash]) {
       return this.entering[hash];
     }
-    const fullHashName = this.cacheNameOfHash(hash);    
-    let result = existsp(fullHashName).then(exists => {
-      if (!exists) {
-        // Create the directory
-        return mkdirpp(fullHashName).then(() => {
-          // Realize the enteries.
-          return Promise.all(entries.map(entry => {
-            if (isDirectory(entry)) {
-               return this.realizeHashedVirtualDirectory(path.join(fullHashName, entry.directory), entry.hash, entry.files);  
-            } else {
-              return this.realize(path.join(fullHashName, entry.file), entry.hash);
-            }
-          }));
-        });
-      }
-    }).then<void>();
-    this.entering[hash] = result;
-    return result;
+    return this.entering[hash] = this.enterHashVirtualDirectoryImpl(hash, entries);
   }
   
-  private realizeHashedVirtualDirectory(location: string, hash: string, entries: Entries): Promise<string> {
+  private async enterHashVirtualDirectoryImpl(hash: string, entries: Entries): Promise<void> {
     const fullHashName = this.cacheNameOfHash(hash);
-    return this.enterHashedVirtualDirectory(hash, entries).then(() => {
-      return link(fullHashName, location);
-    }).then(() => hash);
+    if (!(await existsp(fullHashName))) {
+      await mkdirpp(fullHashName);
+      await Promise.all(entries.map(entry => {
+        if (isDirectory(entry)) {
+          return this.realizeHashedVirtualDirectory(path.join(fullHashName, entry.directory), entry.hash, entry.files);
+        } else {
+          return this.realize(path.join(fullHashName, entry.file), entry.hash);
+        }
+      }));
+    }
+  }
+
+  private async realizeHashedVirtualDirectory(location: string, hash: string, entries: Entries): Promise<string> {
+    const fullHashName = this.cacheNameOfHash(hash);
+    await this.enterHashedVirtualDirectory(hash, entries);
+    await link(fullHashName, location);
+    return hash;
   }
 
   private ensureEntry(directory: string, entry: Entry): Promise<void> {
@@ -155,39 +146,33 @@ export class ContentStore {
       return this.enterHashedFile(fullFileName, entry.hash);
     }
   }
-    
+
   private enterHashedDirectory(directory: string, entries: Entries, hash: string): Promise<any> {
     // A directory entry in the cache is, itself, a directory with its entries realized.
     if (this.entering[hash]) {
       return this.entering[hash];
     }
-    let fullDirectoryName: string;
-    fullDirectoryName = this.cacheNameOfHash(hash);
-    let result = existsp(fullDirectoryName).then(exists => {
-      if (!exists) {
-        // Ensure all the entries are in the cache.
-        return Promise.all(entries.map(entry => this.ensureEntry(directory, entry))).then(() => {
-          // Create a directory directory position.
-          return mkdirpp(fullDirectoryName);
-        }).then(() => {
-          // Realize all the entries into the directory.
-          return Promise.all(entries.map(entry => {
-            let name: string;
-            let hash: string;
-            if (isDirectory(entry)) {
-              name = entry.directory;
-              hash = entry.hash;
-            } else {
-              name = entry.file;
-              hash = entry.hash;
-            }
-            return  this.realize(path.join(fullDirectoryName, name), hash);
-          }));
-        });
-      }
-    }).then<void>();
-    this.entering[hash] = result;
-    return result;
+    return this.entering[hash] = this.enterHashedDirectoryImpl(directory, entries, hash);
+  }
+
+  private async enterHashedDirectoryImpl(directory: string, entries: Entries, hash: string): Promise<any> {
+    let fullDirectoryName = this.cacheNameOfHash(hash);
+    if (!(await existsp(fullDirectoryName))) {
+      await Promise.all(entries.map(entry => this.ensureEntry(directory, entry)));
+      await mkdirpp(fullDirectoryName);
+      await Promise.all(entries.map(entry => {
+        let name: string;
+        let hash: string;
+        if (isDirectory(entry)) {
+          name = entry.directory;
+          hash = entry.hash;
+        } else {
+          name = entry.file;
+          hash = entry.hash;
+        }
+        return this.realize(path.join(fullDirectoryName, name), hash);
+      }));
+    }
   }
   
   private enterHashedFile(filename: string, hash: string): Promise<void> {
@@ -206,6 +191,14 @@ export class ContentStore {
     return result;
   }
   
+  private async enterHashedFileImpl(filename: string, hash: string): Promise<void> {
+    let fullCacheName = this.cacheNameOfHash(hash);
+    if (!(await existsp(fullCacheName))) {
+      await mkdirpp(path.dirname(fullCacheName));
+      await copyFile(filename, fullCacheName);
+    }
+  }
+
   private cacheNameOfHash(hash: string): string {
     return path.join(this.location, hash.substr(0, 2), hash.substr(2,2), hash.substr(4));
   }
